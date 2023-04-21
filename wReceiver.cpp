28d4b47b30e4da787c2388d52bd49852642b465f
xutao <464612404@qq.com>
2023-04-21 23:42:46 +0800

    Add write to file

--- wReceiver.cpp
+++ wReceiver.cpp
@@ -276,7 +276,7 @@ int run_server(int port, int queue_size, int window_size, char * store_dir, cons
                         stream.open(store_file_dir, std::ios_base::app); // open file stream
                         if( !stream )
                             cout << "Opening file failed" << endl;
-                        printf("%ld, %d\n", strlen(data), seq_num);
+//                        printf("%ld, %d\n", strlen(data), seq_num);
                         stream.write(data, len); // write char * into file stream
                         if( !stream )
                             cout << "Write failed" << endl;
